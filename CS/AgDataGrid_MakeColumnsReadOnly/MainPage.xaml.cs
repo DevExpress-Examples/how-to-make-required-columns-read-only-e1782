543278a566f95ac4c066b2a55a4efd5d31a7bf86
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AgDataGrid_MakeColumnsReadOnly/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DevExpress.AgDataGrid;
+using DevExpress.Utils;
+
+namespace AgDataGrid_MakeColumnsReadOnly {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = ProductList.GetData();
+        }
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) {
+            CheckBox cb = sender as CheckBox;
+            MakeColumnReadOnly(FindColumn(cb), DefaultBoolean.False);
+        }
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e) {
+            CheckBox cb = sender as CheckBox;
+            MakeColumnReadOnly(FindColumn(cb), DefaultBoolean.True);
+        }
+        private void MakeColumnReadOnly(AgDataGridColumn col, DefaultBoolean isReadOnly) {
+            if (col != null)
+                col.AllowEditing = isReadOnly;
+        }
+        private AgDataGridColumn FindColumn(CheckBox cb) {
+            if (cb == cbCompanyName)
+                return grid.Columns["CompanyName"];
+            if (cb == cbUnitPrice)
+                return grid.Columns["UnitPrice"];
+            if (cb == cbQuantity)
+                return grid.Columns["Quantity"];
+            return null;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to Make Required Columns Read-Only
+
+
+<p>This example shows how to make specified columns read-only. To prevent an end-user from changing a column's values, check the corresponding CheckBox. If the CheckBox is unchecked, the corresponding column can be edited.</p>
+
+<br/>
+
+
